bd48d939598e9574d1e6fcbeb5f4bdb264277cf0
Cameron <hackwithcameron@gmail.com>
2020-07-13 12:19:01 -0700

    Any Direction
    Rat will now search each direction until solution is found allowing up, down, right, or left movement. Does not necessarily find shortest path.

--- maze.py
+++ maze.py
@@ -31,27 +31,17 @@ maze_4 = [
     [1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
     [1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
-    [0, 1, 1, 1, 2, 1, 0, 1, 0, 1, 1, 1, 2]
+    [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 2]
 ]
 
 
 def print_maze(maze):
     for row in maze:
         print(row)
-    print("- - - - - - - - -")
+    print(" - " * len(maze[0]))
 
 
 def create_blank(maze):
     blank_maze = [[0 for j in range(len(maze[0]))] for i in range(len(maze))]
     return blank_maze
 
-
-def get_maze_size(maze):
-    x = 0
-    y = 0
-    for i in maze:
-        x += 1
-        for j in i:
-            y += 1
-    y //= x
-    return x, y
--- rat.py
+++ rat.py
@@ -1,46 +1,55 @@
-from maze import print_maze, create_blank, get_maze_size,\
+from maze import print_maze, create_blank,\
     maze_1, maze_2, maze_3, maze_4
 
 
 def rat(maze):
-    start_pos = (0, 0)
-    solution = create_blank(maze)
-    maze_height, maze_width = get_maze_size(maze)
-    print(f"Input Maze Height: {maze_height} Width: {maze_width}")
-    print_maze(maze)  # Prints maze
+    """
+    Main function called to find path through maze.
 
-    if not find_path(maze, start_pos, solution, maze_width, maze_height):
+    :param maze: Maze for path to be found
+    :return: Solution to maze or "No solution"
+    """
+    start_pos = (0, 0)
+    solution = create_blank(maze)  # Creates blank maze same size of original
+    print(f"Input Maze \nHeight:{len(maze)} Width:{len(maze[0])}")
+    print_maze(maze)  # Prints input maze
+
+    if not find_path(maze, start_pos, solution):
         return print("No solution!")
     print("Solution")
-    print_maze(solution)
+    return print_maze(solution)
 
 
-def finish(pos, maze, maze_width, maze_height):
+def finish(pos, maze):
     x, y = pos
-    if is_valid(maze, pos, maze_width, maze_height) is True:
+    if is_valid(maze, pos) is True:
         location = maze[x][y]
         if location == 2:
             return True
 
 
-def find_path(maze, pos, solution, maze_width, maze_height):
+def find_path(maze, pos, solution):
     x, y = pos  # Unpacks coordinates
-    if finish(pos, maze, maze_width, maze_height):
+    if finish(pos, maze):
         solution[x][y] = 1
         return True
-    if is_valid(maze, pos, maze_width, maze_height) is True:
+    if is_valid(maze, pos) is True and solution[x][y] != 1:
         solution[x][y] = 1
-        if find_path(maze, (x + 1, y), solution, maze_width, maze_height) is True:
+        if find_path(maze, (x + 1, y), solution) is True:
             return True
-        if find_path(maze, (x, y + 1), solution, maze_width, maze_height) is True:
+        if find_path(maze, (x, y + 1), solution) is True:
+            return True
+        if find_path(maze, (x - 1, y), solution) is True:
+            return True
+        if find_path(maze, (x, y - 1), solution) is True:
             return True
         solution[x][y] = 0
         return False
 
 
-def is_valid(maze, pos, maze_width, maze_height):
+def is_valid(maze, pos):
     x, y = pos
-    if 0 <= x < maze_height and 0 <= y < maze_width and maze[x][y]:
+    if 0 <= x < len(maze) and 0 <= y < len(maze[0]) and maze[x][y]:
         return True
     return False
 
