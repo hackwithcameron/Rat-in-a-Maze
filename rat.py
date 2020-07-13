e91d55a8f2acfb97cff087dbe825b8bffa074b51
Cameron <hackwithcameron@gmail.com>
2020-07-13 11:40:58 -0700

    Get maze size
    Program will now get the height and the width if the input maze helping with errors when maze is not set size.

--- rat.py
+++ rat.py
@@ -1,48 +1,49 @@
-from maze import print_maze, create_blank, maze_1, maze_2
+from maze import print_maze, create_blank, get_maze_size,\
+    maze_1, maze_2, maze_3, maze_4
 
 
 def rat(maze):
     start_pos = (0, 0)
     solution = create_blank(maze)
-    print("Input Maze")
+    maze_height, maze_width = get_maze_size(maze)
+    print(f"Input Maze Height: {maze_height} Width: {maze_width}")
     print_maze(maze)  # Prints maze
 
-    if not find_path(maze, start_pos, solution):
+    if not find_path(maze, start_pos, solution, maze_width, maze_height):
         return print("No solution!")
     print("Solution")
     print_maze(solution)
 
 
-def finish(pos, maze):
+def finish(pos, maze, maze_width, maze_height):
     x, y = pos
-    if is_valid(maze, pos) is True:
+    if is_valid(maze, pos, maze_width, maze_height) is True:
         location = maze[x][y]
         if location == 2:
             return True
 
 
-def find_path(maze, pos, solution):
+def find_path(maze, pos, solution, maze_width, maze_height):
     x, y = pos  # Unpacks coordinates
-    if finish(pos, maze):
+    if finish(pos, maze, maze_width, maze_height):
         solution[x][y] = 1
         return True
-    if is_valid(maze, pos) is True:
+    if is_valid(maze, pos, maze_width, maze_height) is True:
         solution[x][y] = 1
-        if find_path(maze, (x + 1, y), solution) is True:
+        if find_path(maze, (x + 1, y), solution, maze_width, maze_height) is True:
             return True
-        if find_path(maze, (x, y + 1), solution) is True:
+        if find_path(maze, (x, y + 1), solution, maze_width, maze_height) is True:
             return True
         solution[x][y] = 0
         return False
 
 
-def is_valid(maze, pos):
+def is_valid(maze, pos, maze_width, maze_height):
     x, y = pos
-    if 0 <= x < 5 and 0 <= y < 5 and maze[x][y]:
+    if 0 <= x < maze_height and 0 <= y < maze_width and maze[x][y]:
         return True
     return False
 
 
 if __name__ == "__main__":
-    rat(maze_1)
-    rat(maze_2)
+    rat(maze_4)
