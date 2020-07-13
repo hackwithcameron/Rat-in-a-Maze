6d77da044ab11e3f12b5f0153e5388a64402dc95
Cameron <hackwithcameron@gmail.com>
2020-07-13 11:16:21 -0700

    Down and right
    Basic solution to rat-in-a-maze problem using backtracking. rat can only move down or right.

--- maze.py
+++ maze.py
@@ -1,13 +1,27 @@
-
 # 0 = wall, 1 = path/start in top left, 2 = end.
 maze_1 = [
     [1, 0, 0, 0, 0],
-    [1, 1, 1, 0, 2],
+    [1, 1, 1, 0, 0],
     [0, 1, 0, 1, 1],
-    [1, 1, 1, 1, 0]
+    [1, 1, 0, 1, 0],
+    [0, 1, 1, 1, 2]
 ]
+
+maze_2 = [
+    [1, 1, 1, 1, 0],
+    [0, 1, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 1],
+    [1, 1, 0, 0, 2]
+]
+
 
 def print_maze(maze):
     for row in maze:
         print(row)
-    print(" - - - - - - - - - ")
+    print("- - - - - - - - -")
+
+
+def create_blank(maze):
+    blank_maze = [[0 for j in range(len(maze[0]))] for i in range(len(maze))]
+    return blank_maze
--- rat.py
+++ rat.py
@@ -1,44 +1,48 @@
-from maze import print_maze, maze_1
+from maze import print_maze, create_blank, maze_1, maze_2
 
 
-def rat(maze, pos):
-    print_maze(maze)
-    if finish(maze, pos):
+def rat(maze):
+    start_pos = (0, 0)
+    solution = create_blank(maze)
+    print("Input Maze")
+    print_maze(maze)  # Prints maze
+
+    if not find_path(maze, start_pos, solution):
+        return print("No solution!")
+    print("Solution")
+    print_maze(solution)
+
+
+def finish(pos, maze):
+    x, y = pos
+    if is_valid(maze, pos) is True:
+        location = maze[x][y]
+        if location == 2:
+            return True
+
+
+def find_path(maze, pos, solution):
+    x, y = pos  # Unpacks coordinates
+    if finish(pos, maze):
+        solution[x][y] = 1
         return True
-    else:
-        new_position = find_next(maze, pos)
-        if new_position not in pos:
-            rat(maze, new_position)
-
-
-def finish(maze, pos):
-    location = maze[pos[0]][pos[1]]
-    if location == 2:
-        return True
-    else:
+    if is_valid(maze, pos) is True:
+        solution[x][y] = 1
+        if find_path(maze, (x + 1, y), solution) is True:
+            return True
+        if find_path(maze, (x, y + 1), solution) is True:
+            return True
+        solution[x][y] = 0
         return False
 
 
-def find_next(maze, pos):
-    location = maze[pos[0]][pos[1]]
-    down = maze[pos[0] + 1][pos[1]]
-    right = maze[pos[0]][pos[1] + 1]
-    up = maze[pos[0] - 1][pos[1]]
-    left = maze[pos[0]][pos[1] - 1]
-    if up == 1:
-        return pos[0] + 1, pos[1]
-    if right == 1:
-        return right
-    if down == 1:
-        return down
-    if left == 1:
-        return left
-    else:
-        return location
-
-
+def is_valid(maze, pos):
+    x, y = pos
+    if 0 <= x < 5 and 0 <= y < 5 and maze[x][y]:
+        return True
+    return False
 
 
 if __name__ == "__main__":
-    start_pos = (0, 0)
-    rat(maze_1, start_pos)
+    rat(maze_1)
+    rat(maze_2)
